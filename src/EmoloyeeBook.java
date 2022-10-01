3d1fd76f71d9bdf33b39044f70e32fdf023b1ca3
igr1 <dfrgth>
2022-10-01 17:32:40 +1000

    2.7 процесс

--- src/EmoloyeeBook.java
+++ src/EmoloyeeBook.java
@@ -10,116 +10,126 @@ public class EmoloyeeBook {
     public void addEmployee(Employee employee) {
         employees.put(employee.getName(), employee);
     }
-    public void addEmployee(String name,  double salary, int department) {
-        addEmployee(new  Employee(name,  department,salary));
+
+    public void addEmployee(String name, double salary, int department) {
+        addEmployee(new Employee(name, department, salary));
     }
-    public void  changeSalary(Employee employee, int newSalary) {
+
+    public void changeSalary(Employee employee, int newSalary) {
         String key = employee.getName();
         if (employees.containsKey(key)) {
             employees.get(key).setSalary(newSalary);
         }
     }
-   public void  changeDepartment(Employee employee, int newDepartment) {
-       String key = employee.getName();
-       if (employees.containsKey(key)) {
-           employees.get(key).setSalary(newDepartment);
-       }
+
+    public void changeDepartment(Employee employee, int newDepartment) {
+        String key = employee.getName();
+        if (employees.containsKey(key)) {
+            employees.get(key).setSalary(newDepartment);
+        }
     }
+
     public static void allEmployee() {
-            System.out.println(employees.toString());
+        System.out.println(employees.toString());
 
     }
-     public static double salaryAll() {
-        double summ = 0;
-         for (Employee employee:  employees.values()) {
-             summ = summ + (employees.get(employee.getName()).getSalary());
-         }
 
-       System.out.println("Сумма всех зарплат в месяц : " +  summ);
+    public static double salaryAll() {
+        double summ = 0;
+        for (Employee employee : employees.values()) {
+            summ = summ + (employees.get(employee.getName()).getSalary());
+        }
         return summ;
     }
 
-     public static void minimum() {
-        double  min = 100000000;
-         for (Employee employee:  employees.values()) {
-            if (min >  (employees.get(employee.getName())).getSalary()) {
-                min =  (employees.get(employee.getName())).getSalary();
+    public static void minimum() {
+        double min = 100000000;
+        for (Employee employee : employees.values()) {
+            if (min > (employees.get(employee.getName())).getSalary()) {
+                min = (employees.get(employee.getName())).getSalary();
             }
-        } System.out.println("Минимальная зарплата в месяце : " +  min);
+        }
+        System.out.println("Минимальная зарплата в месяце : " + min);
     }
-     public void maximum() {
-        double  max = 0;
-         for (Employee employee:  employees.values()) {
+
+    public void maximum() {
+        double max = 0;
+        for (Employee employee : employees.values()) {
             if (max < (employees.get(employee.getName())).getSalary()) {
-                max =  (employees.get(employee.getName())).getSalary();
+                max = (employees.get(employee.getName())).getSalary();
             }
-        } System.out.println("Максимальная зарплата в месяце : " +  max);
+        }
+        System.out.println("Максимальная зарплата в месяце : " + max);
     }
-    public  void averageSalary() {
+
+    public void averageSalary() {
         int count = employees.values().size();
         if (count != 0) {
-            System.out.println("Средняя зарплата в месяце : " + salaryAll()/count);
+            System.out.println("Средняя зарплата в месяце : " + salaryAll() / count);
         }
     }
-    public  void allStaff() {
+
+    public void allStaff() {
         Set<String> setKey = employees.keySet();
         System.out.println("Список всех сотрудников : ");
         for (String key : setKey) {
-            System.out.println(key );
+            System.out.println(key);
         }
     }
     // Повышенная сложность
 
-    public  void indexing(int x) {
-        double salary1 =0;
-        for (Employee employee:  employees.values()) {
-            salary1 =(employees.get(employee.getName())).getSalary()*x/100;
+    public void indexing(int x) {
+        double salary1 = 0;
+        for (Employee employee : employees.values()) {
+            salary1 = (employees.get(employee.getName())).getSalary() * x / 100;
             (employees.get(employee.getName())).setSalary(salary1);
         }
     }
-    public  void departmentMin(int x) {
+
+    public void departmentMin(int x) {
         double min = Double.MAX_VALUE;
-        String key = null;
-        for (Map.Entry<String , Employee> entry: employees.entrySet()) {
+        for (Map.Entry<String, Employee> entry : employees.entrySet()) {
             Employee employee = entry.getValue();
-            if (employee.getSalary() < min) {
+            if (employee.getDepartment() == x && employee.getSalary() < min) {
                 min = employee.getSalary();
             }
         }
-        if (key != null) {
-            System.out.println("В отделе : " + x +  "Минимальная " +
-                    "зарплата в месяце : " +  employees.get(key));
+        if (min != Double.MAX_VALUE) {
+            System.out.println("В отделе : " + x + "Минимальная " +
+                    "зарплата в месяце : " + min);
         } else System.out.println("нет данных");
     }
+
     public static void departmentMax(int x) {
-        double max = Integer.MIN_VALUE;
-        String key = null;
-        for (Map.Entry<String , Employee> entry: employees.entrySet()) {
+        double max = 0;
+        for (Map.Entry<String, Employee> entry : employees.entrySet()) {
             Employee employee = entry.getValue();
-            if (employee.getSalary() > max) {
+            if (employee.getDepartment() == x && employee.getSalary() > max) {
                 max = employee.getSalary();
             }
         }
-        if (key != null) {
-            System.out.println("В отделе : " + x +  "Максимальная " +
-                    "зарплата в месяце : " +  employees.get(key));
+        if (max != 0) {
+            System.out.println("В отделе : " + x + "Максимальная " +
+                    "зарплата в месяце : " + max);
         } else System.out.println("нет данных");
-    }/*
+    }
     public  void departmentSumm(int x) {
-        int summ = 0;
-        for (int i = 0; i < department.length; i++) {
-            if (x == department[i]) {
-                summ = summ + salary[i];
+        double summ = 0;
+        for (Map.Entry<String, Employee> entry : employees.entrySet()) {
+            Employee employee = entry.getValue();
+            if (x == employee.getDepartment()) {
+                summ = summ + employee.getSalary();
 
             }
         }  System.out.println("В отделе : " + x +  "Сумма зарплат в месяце : " +  summ);
 
     }
     public  void departmentAverage(int x) {
-        int summ = 0, a = 0;
-        for (int i = 0; i < department.length; i++) {
-            if (x == department[i]) {
-                summ = summ + salary[i];
+        double summ = 0, a = 0;
+        for (Map.Entry<String, Employee> entry : employees.entrySet()) {
+            Employee employee = entry.getValue();
+            if (x == employee.getDepartment()) {
+                summ = summ + employee.getSalary();
                 a = a++;
 
             }
@@ -127,10 +137,12 @@ public class EmoloyeeBook {
 
     }
     public  void departmentIndexing(int x,int protsent) {
-        int summ = 0;
-        for (int i = 0; i < department.length; i++) {
-            if (x == department[i]) {
-               salary[i] = salary[i] + salary[i]* protsent/100;
+        double summ = 0 , salary1 =0;
+        for (Employee employee : employees.values()) {
+            if (employee.getDepartment() == x) {
+               salary1 = employee.getSalary() + employee.getSalary() * protsent/100;
+                employee.setSalary(salary1);
+                summ = summ + salary1;
 
             }
         }  System.out.println("В отделе : " + x +  "Сумма зарплат в месяце : " +  summ);
@@ -138,60 +150,41 @@ public class EmoloyeeBook {
     }
     public  void departmentAll(int x) {
         System.out.println(" В отделе " + x + " : ");
-        for (int i = 0; i < department.length; i++) {
-            if (x == department[i]) {
-                System.out.println("Сотрудник : " + name[i] + " Зарплата : " + salary[i]);
+        for (Map.Entry<String, Employee> entry : employees.entrySet()) {
+            Employee employee = entry.getValue();
+            if (x == employee.getDepartment()) {
+                System.out.println("Сотрудник : " + entry.getKey() + " Зарплата : " + employee.getSalary());
 
             }
         }
 
     }
     public  void less(int x) {
-        for (int i = 0; i < salary.length; i++) {
-            if (x > salary[i]) {
-                System.out.println("Сотрудник :  " + name[i] + "id : " + id[i] + " зарплата : " + salary[i]);
-            }
+        System.out.println("Сотрудник с зарплатой меньше, чем "+ x + " :");
+        for (Employee employee : employees.values()) {
+            if (employee.getSalary() < x) {
+                System.out.println(employee.toString());
+                    }
         }
     }
     public  void more(int x) {
-        for (int i = 0; i < salary.length; i++) {
-            if (x <= salary[i]) {
-                System.out.println("Сотрудник :  " + name[i] + "id : " + id[i] + " зарплата : " + salary[i]);
+        System.out.println("Сотрудник с зарплатой больше, чем "+ x + " :");
+        for (Employee employee : employees.values()) {
+            if (employee.getSalary() < x) {
+                System.out.println(employee.toString());
             }
         }
     }
     // присвоение значение id
     public  static void  id(int b) {
         int a = 0;
-        for (int i = 0; i < id.length; i++) {
-            if (a < id[i]) {
-                a = id[i];
+        for (Employee employee : employees.values()) {
+            if (a < employee.getId()) {
+                a = employee.getId();
             }
-            id[b] = a + 1;
 
         }
+        int id  = a + 1;
     }
-    // Добавить нового сотрудника
-    public  static void newStaff(String name, int department1, int salary1) {
-        for (int i = 0; i < department.length; i++) {
-            if (department[i] == 0) {
-                Employee z = new Employee(name,department1,salary1);
-                id(i);
-            }
-        }
-
-    }
-    //удалить сотрудника
-    public static void deleteStaff(int number) {
-        for (int i = 0; i < department.length; i++) {
-            if (id[i] == number) {
-                number = i;
-               Employee z = new Employee("",0,0);
-
-            }
-        }
-
-    }*/
-
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@ public class Main {
 
 
     public static void main(String[] args) {
+        EmoloyeeBook emoloyeeBook = new EmoloyeeBook();
         Employee ira = new Employee("iRA", 2, 23456);
         Employee natali = new Employee("Наталья", 4, 47865);
+        emoloyeeBook.changeSalary(ira,34567);
+        emoloyeeBook.allStaff();
+        System.out.println("Сумма всех зарплат в месяц : " + emoloyeeBook.salaryAll());
 
 
     }
